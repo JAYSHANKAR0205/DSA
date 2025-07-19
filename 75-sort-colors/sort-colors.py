f30c1e14caa59bbca537f47f6de822f689d8099b
JAYSHANKAR0205 <152854072+JAYSHANKAR0205@users.noreply.github.com>
2025-07-19 17:14:10 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.22%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,18 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count0,count1,count2=0,0,0
-        for i in range(len(nums)):
-            if nums[i]==0:
-                count0 += 1
-            elif nums[i] == 1:
-                count1 += 1
-            else:
-                count2 += 1
-        for i in range(count0):
-            nums[i]=0
-        for i in range(count0,count0+count1):
-            nums[i]=1
-        for i in range(count0+count1,len(nums)):
-            nums[i]=2
+        # Used Dutch National Flag Algorithm
+        low, mid, high = 0, 0, len(nums)-1
+        while mid<=high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
         return nums
