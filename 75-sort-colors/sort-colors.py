eb24c8591ccf4b16272cf7ade710d89d2ddd1bd3
JAYSHANKAR0205 <152854072+JAYSHANKAR0205@users.noreply.github.com>
2025-07-19 16:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (33.26%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count0,count1,count2=0,0,0
+        for i in range(len(nums)):
+            if nums[i]==0:
+                count0 += 1
+            elif nums[i] == 1:
+                count1 += 1
+            else:
+                count2 += 1
+        for i in range(count0):
+            nums[i]=0
+        for i in range(count0,count0+count1):
+            nums[i]=1
+        for i in range(count0+count1,len(nums)):
+            nums[i]=2
+        return nums
